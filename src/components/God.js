de3afc55ecc90165487cc0b07dd7fc6d93089b87
Avrosh K <avroshkumar@gmail.com>
2020-07-03 03:55:14 -0400

    Draggable actor

--- src/components/God.js
+++ src/components/God.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 import './God.css';
+import { RiTimerFlashLine } from 'react-icons/ri';
+import { BsMusicNote } from 'react-icons/bs';
 
-const God = ({act}) => {
-  return <div className="God" onClick={(e) => {e.preventDefault(); act();}}>
-  </div>;
+const God = ({act, actInProgress, setActInProgress, clock, music, toggleMusic}) => {
+  return (
+    <Draggable
+      onStart={() => setActInProgress(true)}
+      onStop={() => setActInProgress(false)}
+      bounds="body">
+      <div className={'God '+ (actInProgress ? 'acting':'')}>
+        <div className='handle'></div>
+        <BsMusicNote className={'music-switch '+(music ? '':'stopped')}
+          onClick={() => toggleMusic()} />
+        <RiTimerFlashLine
+          className={'time-switch '+(clock ? '':'stopped')}
+          onClick={() => act()} />
+      </div>
+    </Draggable>
+
+  );
 }
 
 export default God;
